37b071392802283b1393e172f8bf63cb7350c8fa
Hanss <hjimenezm1@miumg.edu.gt>
2022-09-29 04:04:07 -0600

    un chingo de cambios

--- avanzado/urls.py
+++ avanzado/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from simple_app import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('^$', views.IndexView.as_view())
+    url('^$', views.IndexView.as_view()),
+    url('^simple_app/', include('simple_app.urls', namespace= 'simple_app'))
     # url(r'^$', views.index)
 ]
--- simple_app/views.py
+++ simple_app/views.py
@@ -3,16 +3,19 @@ from django.views.generic import View, TemplateView, ListView, DetailView
 from simple_app import models
 # from django.http import HttpResponse
 
+class IndexView(TemplateView):
+    template_name =  'index.html'
 
 class SchoolListView(ListView):
+    context_object_name = 'schools'
     model = models.School
-    
+
 class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
     model = models.School
     template_name = 'simple_app/school_detail.html'
-    
-# class IndexView(TemplateView):
-#     template_name =  'index.html'
+
+
 
 #     def get_context_data(self, **kwargs):
 #         context = super().get_context_data(**kwargs)
