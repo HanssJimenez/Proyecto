ba3209156400404f1c0e9813a57754c7fb35cef5
Hanss <hjimenezm1@miumg.edu.gt>
2022-09-29 03:16:52 -0600

    hola

--- simple_app/views.py
+++ simple_app/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView, DetailView
+from simple_app import models
 # from django.http import HttpResponse
 
-class IndexView(TemplateView):
-    template_name =  'index.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['ingresa'] = 'ingreso basico'
-        return context
+class SchoolListView(ListView):
+    model = models.School
+    
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name = 'simple_app/school_detail.html'
+    
+# class IndexView(TemplateView):
+#     template_name =  'index.html'
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['ingresa'] = 'ingreso basico'
+#         return context
+
 # class CBView(View):
 #     def get(sefl,request):
 #         return HttpResponse("Vista de clases es increible")
